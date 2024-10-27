e1b629d9de39dcf59cf9499ca969f500dc7dc0c1
ashu16012002 <ashutoshtamrakar57@gmail.com>
2024-10-27 17:54:56 +0530

    update

--- ashuportfolio/settings.py
+++ ashuportfolio/settings.py
@@ -122,9 +122,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "home/static"]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "home/static",
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
